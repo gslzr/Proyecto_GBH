c01957586278b20bfde06ec4b81f7d7cdd7df6f7
Guadalupe <gsalazar@correo.unimet.edu.ve>
2024-08-10 09:32:39 -0400

    actualice main

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-def main():
-    print('este es nuestro proyecto')
-main()
+import requests
+from moduloEspecie import moduloEspecie
+from moduloPelicula import moduloPelicula
+response = requests.get("https://www.swapi.tech/api/species")
+
+
+print("a")
+
