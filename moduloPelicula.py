b4a493fde2e53a09630aeb8b5692a853599a8887
Guadalupe <gsalazar@correo.unimet.edu.ve>
2024-08-12 08:37:08 -0400

    los commits que me faltaron anoche

--- moduloPelicula.py
+++ moduloPelicula.py
@@ -25,7 +25,24 @@ class moduloPelicula:
             for especie in self.peliculas["result"][i]["properties"]["species"]: 
                 link = requests.get(especie)
                 info_especie = link.json()
-                lista_especies.append(info_especie["result"]["properties"]["classification"])  
+                #Se obtiene el nombre de la especie
+                nombre_especie = info_especie["result"]["properties"]["classification"] 
+                #Se verifica que la especie no se encuentre ya en la lista
+                if nombre_especie not in lista_especies:
+                    #Se guarda la especie en la lista de especies que aparecen en la pelicula
+                    lista_especies.append(nombre_especie) 
+
+            #Se guardan los nombres de los planetas que intervienen en la pelicula
+            lista_planetas = []
+            for planeta in self.peliculas["result"][i]["properties"]["planets"]: 
+                link = requests.get(planeta)
+                info_planeta = link.json()
+                #Se obtiene el nombre del planeta
+                nombre_planeta = info_planeta["result"]["properties"]["name"] 
+                #Se verifica que el planeta no se encuentre ya en la lista
+                if nombre_planeta not in lista_planetas:
+                    #Se guarda la especie en la lista de especies que aparecen en la pelicula
+                    lista_planetas.append(nombre_planeta) 
 
             self.lista_peliculas.append(Pelicula(self.peliculas["result"][i]["properties"]["title"], 
                                             self.peliculas["result"][i]["properties"]["episode_id"],
@@ -33,9 +50,12 @@ class moduloPelicula:
                                             self.peliculas["result"][i]["properties"]["opening_crawl"],
                                             self.peliculas["result"][i]["properties"]["director"],
                                             lista_personajes,
-                                            lista_especies
+                                            lista_especies, 
+                                            lista_planetas
                                             ))
-
+'''
 x = moduloPelicula(response)
 for i in x.lista_peliculas:
     i.mostrar_pelicula()
+
+'''
