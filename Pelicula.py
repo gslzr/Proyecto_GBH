b9034d96cf3f3a4f226a49ca59c2867c65234ced
Guadalupe <gsalazar@correo.unimet.edu.ve>
2024-08-12 10:50:28 -0400

    self.planetas no estaba declarado

--- Pelicula.py
+++ Pelicula.py
@@ -1,5 +1,5 @@
 class Pelicula:
-    def __init__(self,titulo, num_episodio, fecha_lanzamiento, opening_crawl, director, personajes, especies):
+    def __init__(self,titulo, num_episodio, fecha_lanzamiento, opening_crawl, director, personajes, especies, planetas):
         self.titulo = titulo
         self.num_episodio = num_episodio
         self.fecha_lanzamiento = fecha_lanzamiento
@@ -7,6 +7,7 @@ class Pelicula:
         self.director = director
         self.personajes = personajes
         self.especies = especies
+        self.planetas = planetas
     
     def mostrar_pelicula(self):
-        print("\nTitulo: ", self.titulo, "\nNúmero de episodio: ", self.num_episodio, "\nFecha de lanzamiento: ", self.fecha_lanzamiento, "\nOpening Crawl: ", self.opening_crawl, "\nDirector: ", self.director, "\nPersonajes: ", self.personajes, "\nEspecies: ", self.especies)
+        print("\nTitulo: ", self.titulo, "\nNúmero de episodio: ", self.num_episodio, "\nFecha de lanzamiento: ", self.fecha_lanzamiento, "\nOpening Crawl: ", self.opening_crawl, "\nDirector: ", self.director, "\nPersonajes: ", self.personajes, "\nEspecies: ", self.especies, "\nPlanetas: ", self.planetas)
